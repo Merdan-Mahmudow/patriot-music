5031d40fa2000ede1353374245a88974a435fc32
Merdan-Mahmudow <softprank@gmail.com>
2025-03-26 23:29:32 +0300

    renamed:    src/background_image.JPEG -> src/assets/background_image.JPEG 	modified:   src/components/Video/index.tsx 	modified:   src/routes/index.css

--- src/components/Video/index.tsx
+++ src/components/Video/index.tsx
@@ -11,19 +11,39 @@ function Video() {
     queryKey: ['video'],
     queryFn: getVideo,
   })
+
+  const videoUrl = data?.url;
   useEffect(() => {
-    if (!data?.url || !playerRef.current) return;
+    if (!videoUrl) return;
+    const player = new Plyr('#player', { autoplay: false });
+    // Bind event listeners.
+    function on({selector, type, callback}: {selector: string, type: string, callback: EventListener}) {
+      const element = document.querySelector(selector);
+      if (element) {
+        element.addEventListener(type, callback, false);
+      }
+    }
+    on({selector: '.js-play', type: 'click', callback: () => { player.play(); }});
+    on({selector: '.js-pause', type: 'click', callback: () => { player.pause(); }});
+    on({selector: '.js-stop', type: 'click', callback: () => { player.stop(); }});
+    on({selector: '.js-rewind', type: 'click', callback: () => { player.rewind(); }});
+    on({selector: '.js-forward', type: 'click', callback: () => { player.forward(); }});
+  }, [videoUrl]);
 
-    const player = new Plyr(playerRef.current, { autoplay: false });
 
-    return () => {
-      player.destroy();
-    };
-  }, [data]);
+  // useEffect(() => {
+  //   if (!data?.url || !playerRef.current) return;
+
+  //   const player = new Plyr(playerRef.current, { autoplay: false });
+
+  //   return () => {
+  //     player.destroy();
+  //   };
+  // }, [data]);
 
   return (
-    <div className="flex justify-center items-center h-screen w-full bg-gray-800">
-      <video src={data?.url} width={576} controls crossOrigin="anonymous" playsInline poster="/poster.png" id="player">
+    <div className="flex justify-center items-center w-full bg-transparent bg-gray-800">
+      <video src={data?.url} width={576} height={200}  crossOrigin="anonymous" playsInline poster="/poster.png" id="player">
       </video>
     </div>
   );
